94181b2d11a2f61dbed09503d9c604086f048d9b
tofighi <ghassem+github@gmail.com>
2018-02-24 01:30:15 -0500

    first commit

---
+++ lang/ru/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['cache']                 = 'Отключить кэширование';
+$lang['toolbar_inserted_markup'] = 'Разметка для вставки через кнопку на панели инструментов страницы';
